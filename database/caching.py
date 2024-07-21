4f006e6b18c03e7524bf614418963486da9b924d
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-07-22 00:20:02 +0500

    added data caching

---
+++ database/caching.py
@@ -0,0 +1,19 @@
+from database.redis import RedisPool
+
+redis = RedisPool()
+
+
+async def in_cache(key: str) -> bool:
+    conn = await redis.get_connection()
+    return await conn.get(key) is not None
+
+
+async def record_in_cache(key: str, value: str) -> None:
+    conn = await redis.get_connection()
+    await conn.set(key, value)
+
+
+async def get_from_cache(key: str) -> str:
+    conn = await redis.get_connection()
+    value = await conn.get(key)
+    return value.decode('utf-8')
